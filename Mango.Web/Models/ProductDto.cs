5da29380f5874b048591cedab27322d0e9c10435
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-03 16:28:40 -0700

    Details View

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -1,5 +1,7 @@
 ﻿namespace Mango.Web.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class ProductDto
 {
     public int ProductId { get; set; }
@@ -8,4 +10,6 @@ public class ProductDto
     public string Description { get; set; }
     public string CategoryName { get; set; }
     public string ImageUrl { get; set; }
+    [Range(1, 100)] 
+    public int Count { get; set; } = 1;
 }
