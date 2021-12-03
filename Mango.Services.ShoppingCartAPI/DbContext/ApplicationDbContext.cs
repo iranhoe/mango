42a3c37c9c9f621aefa567a3177f78654e57bb3c
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-03 16:50:15 -0700

    Setup startup class file for shopping cart

---
+++ Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Services.ShoppingCartAPI.DbContext;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    // public DbSet<Product> Products { get; set; }
+}
---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Mango.Services.ShoppingCartAPI;
+
+using AutoMapper;
+
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+        {
+            // config.CreateMap<ProductDto, Product>();
+            // config.CreateMap<Product, ProductDto>();
+        });
+
+        return mappingConfig;
+    }
+}
