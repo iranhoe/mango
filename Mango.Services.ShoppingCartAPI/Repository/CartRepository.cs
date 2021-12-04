2bca242674b3c8d1446e20d7ef990f98d2b2d0f8
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-03 18:29:49 -0700

    Add to Shopping Cart Repository Method

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -1,9 +1,16 @@
 ﻿namespace Mango.Services.ShoppingCartAPI.Repository;
 
+using AutoMapper;
+using DbContext;
+using Microsoft.EntityFrameworkCore;
+using Models;
 using Models.Dto;
 
 public class CartRepository : ICartRepository
 {
+    private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
+    
     public async Task<CartDto> GetCartByUserId(string userId)
     {
         throw new NotImplementedException();
@@ -11,7 +18,28 @@ public class CartRepository : ICartRepository
 
     public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
     {
-        throw new NotImplementedException();
+        Cart cart = _mapper.Map<Cart>(cartDto);
+
+        var prodInDb = _db.Products.FirstOrDefault(u => u.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+
+        if (prodInDb == null)
+        {
+            _db.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+            await _db.SaveChangesAsync();
+        }
+
+        var cartHeaderFromDb = _db.CartHeader
+            .FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
+
+        if (cartHeaderFromDb == null)
+        {
+            _db.CartHeader.Add(cart.CartHeader);
+            await _db.SaveChangesAsync();
+            cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+            cart.CartDetails.FirstOrDefault().Product = null;
+            _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+            await _db.SaveChangesAsync();
+        }
     }
 
     public async Task<bool> RemoveFromCart(int cartDetailsId)
