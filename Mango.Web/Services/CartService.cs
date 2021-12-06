31d66857d5cceb1fc18f09627403093ef8a34c93
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-06 11:03:51 -0700

    81. Configure Cart Service

---
+++ Mango.Web/Services/CartService.cs
@@ -0,0 +1,59 @@
+﻿namespace Mango.Web.Services;
+
+using IServices;
+using Models;
+
+public class CartService : BaseService, ICartService
+{
+    private readonly IHttpClientFactory _clientFactory;
+    
+    public CartService(IHttpClientFactory clientFactory) : base(clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<T> GetCartByUserIdAsync<T>(string userId, string token = null)
+    {
+        return await this.SendAsync<T>(new ApiRequest()
+        {
+            ApiType = SD.ApiType.GET,
+            Url = SD.ShoppingCartAPIBase + "api/cart/GetCart/" + userId,
+            AccessToken = token
+        });
+    }
+
+    public async Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null)
+    {
+        return await this.SendAsync<T>(new ApiRequest()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = cartDto,
+            Url = SD.ShoppingCartAPIBase + "api/cart/AddCart",
+            AccessToken = token
+        });
+    }
+
+    public async Task<T> UpdatreCartAsync<T>(CartDto cartDto, string token = null)
+    {
+        return await this.SendAsync<T>(new ApiRequest()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = cartDto,
+            Url = SD.ShoppingCartAPIBase + "api/cart/UpdateCart",
+            AccessToken = token
+        });
+    }
+
+    public async Task<T> RemoveFromCartAsync<T>(int cartId, string token = null)
+    {
+        return await this.SendAsync<T>(new ApiRequest()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = cartId,
+            Url = SD.ShoppingCartAPIBase + "api/cart/RemoveCart",
+            AccessToken = token
+        });
+    }
+
+  
+}
