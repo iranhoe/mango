3838d32c53dd42eff6d1b18b4aa0e8510fcb9e8d
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-06 13:31:02 -0700

    85. Add Cart Controller

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿namespace Mango.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Services.IServices;
+
+public class CartController : Controller
+{
+    private readonly IProductService _productService;
+    private readonly ICartService _cartService;
+
+    public CartController(IProductService productService, ICartService cartService)
+    {
+        _productService = productService;
+        _cartService = cartService;
+    }
+
+    public IActionResult Index()
+    {
+        
+    }
+}
--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,118 +1,116 @@
-﻿using Mango.Web.Models;
+﻿namespace Mango.Web.Controllers;
+
+using Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Services.IServices;
 
-namespace Mango.Web.Controllers
+public class HomeController : Controller
 {
-    using System.Text.Json;
-    using Microsoft.AspNetCore.Authentication;
-    using Microsoft.AspNetCore.Authorization;
-    using Services.IServices;
+    private readonly IProductService _productService;
+    private readonly ICartService _cartService;
 
-    public class HomeController : Controller
+    public HomeController(IProductService productService, ICartService cartService)
     {
-        private readonly IProductService _productService;
-        private readonly ICartService _cartService;
+        _productService = productService;
+        _cartService = cartService;
+    }
 
-        public HomeController(IProductService productService, ICartService cartService)
-        {
-            _productService = productService;
-            _cartService = cartService;
-        }
-
-        public async Task<IActionResult> Index()
-        {
+    public async Task<IActionResult> Index()
+    {
             
-            List<ProductDto>? list = new();
-            var response = await _productService.GetAllProductsAsync<ResponseDto>("");
-            if (response is {IsSuccess: true})
-            {
-                list = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result) ?? string.Empty, 
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            } 
+        List<ProductDto>? list = new();
+        var response = await _productService.GetAllProductsAsync<ResponseDto>("");
+        if (response is {IsSuccess: true})
+        {
+            list = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result) ?? string.Empty, 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        } 
             
-            return View(list);
-        }
+        return View(list);
+    }
         
-        [Authorize]
-        public async Task<IActionResult> Details(int productId)
+    [Authorize]
+    public async Task<IActionResult> Details(int productId)
+    {
+            
+        ProductDto? model = new();
+        var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+        if (response is {IsSuccess: true})
         {
+            model = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result) ?? string.Empty, 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        } 
             
-            ProductDto? model = new();
-            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
-            if (response is {IsSuccess: true})
-            {
-                model = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result) ?? string.Empty, 
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            } 
-            
-            return View(model);
-        }
+        return View(model);
+    }
         
-        [HttpPost]
-        [ActionName("Details")]
-        [Authorize]
-        public async Task<IActionResult> DetailsPost(ProductDto productDto)
-        {
-            string userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+    [HttpPost]
+    [ActionName("Details")]
+    [Authorize]
+    public async Task<IActionResult> DetailsPost(ProductDto productDto)
+    {
+        string userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
             
-            CartDto cartDto = new()
-            {
-                CartHeader = new CartHeaderDto()
-                {
-                    UserId = userId,
-                    CouponCode = string.Empty
-                }
-            };
-
-            CartDetailsDto cartDetails = new CartDetailsDto()
-            {
-                Count = productDto.Count,
-                ProductId = productDto.ProductId,
-            };
-
-            var resp = await _productService.GetProductByIdAsync<ResponseDto>(productDto.ProductId, "");
-            if (resp != null && resp.IsSuccess)
-            {
-                cartDetails.Product = 
-                    JsonSerializer.Deserialize<ProductDto>(Convert.ToString(resp.Result), 
-                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
-            }
-
-            List<CartDetailsDto> cartDetailsDtos = new();
-            cartDetailsDtos.Add(cartDetails);
-            cartDto.CartDetails = cartDetailsDtos;
-
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var addToCartResp = await _cartService.AddToCartAsync<ResponseDto>(cartDto, accessToken);
-            if (addToCartResp != null && addToCartResp.IsSuccess)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            
-            return View(productDto);
-        }
-
-        public IActionResult Privacy()
+        CartDto cartDto = new()
         {
-            return View();
-        }
+            CartHeader = new CartHeaderDto()
+            {
+                UserId = userId,
+                CouponCode = string.Empty
+            }
+        };
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        CartDetailsDto cartDetails = new CartDetailsDto()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            Count = productDto.Count,
+            ProductId = productDto.ProductId,
+        };
+
+        var resp = await _productService.GetProductByIdAsync<ResponseDto>(productDto.ProductId, "");
+        if (resp != null && resp.IsSuccess)
+        {
+            cartDetails.Product = 
+                JsonSerializer.Deserialize<ProductDto>(Convert.ToString(resp.Result), 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
         }
 
-        [Authorize]
-        public IActionResult Login()
+        List<CartDetailsDto> cartDetailsDtos = new();
+        cartDetailsDtos.Add(cartDetails);
+        cartDto.CartDetails = cartDetailsDtos;
+
+        var accessToken = await HttpContext.GetTokenAsync("access_token");
+        var addToCartResp = await _cartService.AddToCartAsync<ResponseDto>(cartDto, accessToken);
+        if (addToCartResp != null && addToCartResp.IsSuccess)
         {
             return RedirectToAction(nameof(Index));
         }
+            
+        return View(productDto);
+    }
 
-        public IActionResult Logout()
-        {
-            return SignOut("Cookies", "oidc");
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [Authorize]
+    public IActionResult Login()
+    {
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Logout()
+    {
+        return SignOut("Cookies", "oidc");
     }
 }
