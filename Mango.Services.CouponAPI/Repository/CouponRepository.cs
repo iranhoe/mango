bfeafc012191252745b5b16af0f0ed24e7ee23c6
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-06 16:42:09 -0700

    94. Coupon Repository

---
+++ Mango.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Mango.Services.CouponAPI.Repository;
+
+using AutoMapper;
+using DbContexts;
+using Models.Dto;
+
+public class CouponRepository : ICouponRepository
+{
+    private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
+
+    public CouponRepository(ApplicationDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task<CouponDto> GetCouponByCode(string couponCode)
+    {
+        var couponFromDb = _db.Coupons.FirstOrDefault(u => u.CouponCode == couponCode);
+        return _mapper.Map<CouponDto>(couponFromDb);
+    }
+}
---
+++ Mango.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.CouponAPI.Repository;
+
+using Microsoft.EntityFrameworkCore;
+using Models.Dto;
+
+public interface ICouponRepository
+{
+    Task<CouponDto> GetCouponByCode(string couponCode);
+}
