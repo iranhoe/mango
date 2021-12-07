1c62166986a9fe38acf2be995c4ce56c9ceffb84
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-07 16:05:23 -0700

    122. Startup file and Migrations

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Mango.Services.OrderAPI;
+
+using AutoMapper;
+
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+        {
+            // config.CreateMap<CouponDto, Coupon>().ReverseMap();
+            // config.CreateMap<CartHeaderDto, CartHeader>().ReverseMap();
+            // config.CreateMap<CartDetailsDto, CartDetails>().ReverseMap();
+            // config.CreateMap<CartDto, Cart>().ReverseMap();
+        });
+
+        return mappingConfig;
+    }
+}
