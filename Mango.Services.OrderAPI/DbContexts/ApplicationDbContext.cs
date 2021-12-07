8f28d66268966881a50574986b984049a5c534b7
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-07 16:00:31 -0700

    121. Adding Models for Order API

---
+++ Mango.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Services.OrderAPI.DbContexts;
+
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
+}
---
+++ Mango.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace Mango.Services.OrderAPI.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class OrderDetails
+{
+    [Key]
+    public int OrderDetailsId { get; set; }
+    public int OrderHeaderId { get; set; }
+    [ForeignKey("OrderHeaderId")]
+    public virtual OrderHeader? OrderHeader { get; set; }
+    public int ProductId { get; set; }
+    public int Count { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+}
