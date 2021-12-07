abad1308a56d706b74d675216d08acc0bf4e8edb
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-07 11:40:41 -0700

    116. Message Bus Sender Implementation.

---
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,26 @@
+﻿namespace Mango.MessageBus;
+
+using System.Text;
+using System.Text.Json;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+
+public class AzureServiceBusMessageBus : IMessageBus
+{
+    //can be improved
+    private string connectionString = "";
+    public async Task PublishMessage(BaseMessage message, string topicName)
+    {
+        ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+        var JsonMessage = JsonSerializer.Serialize(message);
+        var finalMessage = new Message(Encoding.UTF8.GetBytes(JsonMessage))
+        {
+            CorrelationId = Guid.NewGuid().ToString()
+        };
+
+        await senderClient.SendAsync(finalMessage);
+
+        await senderClient.CloseAsync();
+    }
+}
