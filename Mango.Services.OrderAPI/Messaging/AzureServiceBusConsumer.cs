ce8da9509a50a7025279d0048f016bba8c484c9e
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-07 17:04:42 -0700

    126. Create Models for Azure Messaging

---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,18 @@
+﻿namespace Mango.Services.OrderAPI.Messaging;
+
+using System.Text;
+using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+using Messages;
+
+public class AzureServiceBusConsumer
+{
+    private async Task OnCeckOutMessageReceived(ProcessMessageEventArgs args)
+    {
+        var message = args.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+
+        CheckoutHeaderDto checkoutHeaderDto = JsonSerializer.Deserialize<CheckoutHeaderDto>(body,
+            new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+    }
+}
