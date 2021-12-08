33fa6395c45662f0db8a34cefd0212e2360cbeb0
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-07 17:13:11 -0700

    127. Process Checkout Message and Create Order Details in Database

--- Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -4,9 +4,18 @@ using System.Text;
 using System.Text.Json;
 using Azure.Messaging.ServiceBus;
 using Messages;
+using Models;
+using Repository;
 
 public class AzureServiceBusConsumer
 {
+    private readonly OrderRepository _orderRepository;
+
+    public AzureServiceBusConsumer(OrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
     private async Task OnCeckOutMessageReceived(ProcessMessageEventArgs args)
     {
         var message = args.Message;
@@ -14,5 +23,39 @@ public class AzureServiceBusConsumer
 
         CheckoutHeaderDto checkoutHeaderDto = JsonSerializer.Deserialize<CheckoutHeaderDto>(body,
             new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+
+        OrderHeader orderHeader = new()
+        {
+            UserId = checkoutHeaderDto.UserId,
+            FirstName = checkoutHeaderDto.FirstName,
+            LastName = checkoutHeaderDto.LastName,
+            OrderDetails = new List<OrderDetails>(),
+            CardNumber = checkoutHeaderDto.CardNumber,
+            CouponCode = checkoutHeaderDto.CouponCode,
+            CVV = checkoutHeaderDto.CVV,
+            DiscountTotal = checkoutHeaderDto.DiscountTotal,
+            Email = checkoutHeaderDto.Email,
+            ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+            OrderTime = DateTime.Now,
+            OrderTotal = checkoutHeaderDto.OrderTotal,
+            PaymentStatus = false,
+            Phone = checkoutHeaderDto.Phone,
+            PickupDateTime = checkoutHeaderDto.PickupDateTime
+        };
+
+        foreach (var detailsList in checkoutHeaderDto.CartDetails)
+        {
+            OrderDetails orderDetails = new()
+            {
+                ProductId = detailsList.ProductId,
+                ProductName = detailsList.Product.Name,
+                Price = detailsList.Product.Price,
+                Count = detailsList.Count
+            };
+            orderHeader.CartTotalItems += detailsList.Count;
+            orderHeader.OrderDetails.Add(orderDetails);
+        }
+
+        await _orderRepository.AddOrder(orderHeader);
     }
 }
