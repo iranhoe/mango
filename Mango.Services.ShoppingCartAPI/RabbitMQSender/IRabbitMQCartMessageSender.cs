7433fca87e32ce1b96d4650a87da3eed31bca33a
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-13 15:08:57 -0700

    168. Add RabbitMQ NuGet to Project

---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/IRabbitMQCartMessageSender.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.ShoppingCartAPI.RabbitMQSender;
+
+using MessageBus;
+
+public interface IRabbitMQCartMessageSender
+{
+    void SendMessage(BaseMessage baseMessage, string queueName);
+}
---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.ShoppingCartAPI.RabbitMQSender;
+
+using MessageBus;
+
+public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+{
+    public void SendMessage(BaseMessage baseMessage, string queueName)
+    {
+        throw new NotImplementedException();
+    }
+}
