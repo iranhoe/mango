2ca27b1c1ed24719a68ff47ac38cea00456c33be
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-13 16:59:40 -0700

    177. Publish to Fanout

--- Mango.Services.PaymentAPI/RabbitMQSender/RabbitMqPaymentMessageSender.cs
+++ Mango.Services.PaymentAPI/RabbitMQSender/RabbitMqPaymentMessageSender.cs
@@ -11,6 +11,7 @@ public class RabbitMqPaymentMessageSender : IRabbitMqPaymentMessageSender
     private readonly string _password;
     private readonly string _username;
     private IConnection _connection;
+    private const string ExchangeName = "PublishSubscribePaymentUpdate_Exchange";
 
     public RabbitMqPaymentMessageSender()
     {
@@ -24,10 +25,11 @@ public class RabbitMqPaymentMessageSender : IRabbitMqPaymentMessageSender
         if (ConnectionExist())
         {
             using var channel = _connection.CreateModel();
-            channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+            // channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+            channel.ExchangeDeclare(ExchangeName, ExchangeType.Fanout, false);
             var json = JsonSerializer.Serialize(message, message.GetType());
             var body = Encoding.UTF8.GetBytes(json);
-            channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+            channel.BasicPublish(exchange: ExchangeName, routingKey: "", basicProperties: null, body: body);
         }
     }
 
