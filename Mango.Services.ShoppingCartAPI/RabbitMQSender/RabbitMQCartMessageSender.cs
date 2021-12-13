330382311a5d08067a5811d89f184133ab3e9341
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-12-13 15:17:24 -0700

    169. Implement RabbitMQ Cart Message Sender

--- Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -1,11 +1,35 @@
 ﻿namespace Mango.Services.ShoppingCartAPI.RabbitMQSender;
 
+using System.Text.Json;
 using MessageBus;
+using RabbitMQ.Client;
 
 public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
 {
-    public void SendMessage(BaseMessage baseMessage, string queueName)
+    private readonly string _hostname;
+    private readonly string _password;
+    private readonly string _username;
+    private IConnection _connection;
+
+    public RabbitMQCartMessageSender()
     {
-        throw new NotImplementedException();
+        _hostname = "localhost";
+        _password = "guest";
+        _username = "guest";
+    }
+    
+    public void SendMessage(BaseMessage message, string queueName)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _hostname,
+            UserName = _username,
+            Password = _password
+        };
+        _connection = factory.CreateConnection();
+        using var channel = _connection.CreateModel();
+        channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+        var body = JsonSerializer.SerializeToUtf8Bytes(message);
+        channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties:null, body: body);
     }
 }
